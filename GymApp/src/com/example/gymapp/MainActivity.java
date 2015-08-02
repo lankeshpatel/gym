03e3ab5ec24f60074141cecd1e50de8d71b1294d
Lankesh Patel <lpatel7@uwo.ca>
2015-08-02 14:29:08 -0400

    pedometer

--- GymApp/src/com/example/gymapp/MainActivity.java
+++ GymApp/src/com/example/gymapp/MainActivity.java
@@ -1,17 +1,34 @@
 package com.example.gymapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
+	private Button workout1;
+	private Button workout2;
+	private Button workout3;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        workout1 = (Button) findViewById(R.id.button1);
+		workout2 = (Button) findViewById(R.id.button2);
+		workout3 = (Button) findViewById(R.id.button3);
+
+		workout1.setOnClickListener(this);
+		workout2.setOnClickListener(this);
+		workout3.setOnClickListener(this);
+        
     }
 
     @Override
@@ -31,5 +48,10 @@ public class MainActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+    
+    public void onClick(View v) {
+    	Intent pedometerActivity = new Intent(MainActivity.this, pedometer.class);
+    	startActivity(pedometerActivity);
     }
 }
